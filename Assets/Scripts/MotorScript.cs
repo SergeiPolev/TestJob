043b64bb6559eb098bb488bbdcc78db32988f56b
SergeiPolev <55839857+SergeiPolev@users.noreply.github.com>
2020-10-22 17:39:58 +0300

    Worked up

---
+++ Assets/Scripts/MotorScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorScript : MonoBehaviour
+{
+    public float rotationSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, rotationSpeed, 0, Space.World);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,18 +9,25 @@ public class PlayerMovement : MonoBehaviour
 
     public Transform rightCheck;
     public Transform leftCheck;
+    public Transform groundCheck;
     public LayerMask layerMaskGround;
-
     public GameScreenMovement game;
+
+    bool jumped = false;
+
+    Rigidbody rb;
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
         var leftCheckSphere = Physics.OverlapSphere(leftCheck.position, 0.2f, layerMaskGround);
         var rightCheckSphere = Physics.OverlapSphere(rightCheck.position, 0.2f, layerMaskGround);
+        var groundCheckSphere = Physics.OverlapSphere(groundCheck.position, 0.2f, layerMaskGround);
+
         if (Input.GetKey(KeyCode.LeftArrow) && leftCheckSphere.Length > 0)
         {
             transform.localPosition = new Vector3(transform.localPosition.x - playerSpeed * Time.deltaTime, transform.localPosition.y, transform.localPosition.z);
@@ -29,6 +36,11 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow) && rightCheckSphere.Length > 0)
         {
             transform.localPosition = new Vector3(transform.localPosition.x + playerSpeed * Time.deltaTime, transform.localPosition.y, transform.localPosition.z);
+        }
+
+        if (groundCheckSphere.Length > 0 && !jumped)
+        {
+            rb.velocity = new Vector2(0f, 0f);
         }
     }
     private void OnTriggerEnter(Collider other)
@@ -43,6 +55,20 @@ public class PlayerMovement : MonoBehaviour
                 game.IncreaseDiamonds(1);
                 Destroy(other.gameObject);
                 break;
+            case "Thorn":
+                Destroy(gameObject);
+                break;
+            case "Jump Pad":
+                StartCoroutine(Jumping());
+                rb.AddForce(new Vector3(0f, 65f, 0f), ForceMode.Impulse);
+                break;
         }
+    }
+
+    IEnumerator Jumping()
+    {
+        jumped = true;
+        yield return new WaitForSeconds(0.5f);
+        jumped = false;
     }
 }
